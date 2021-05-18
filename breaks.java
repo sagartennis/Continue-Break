2bc51521c8a5126c87d839458e37de0c6ba264ab
sagartennis <sagarthetennis10k@gmail.com>
2021-05-18 12:55:32 +0530

    Add files via upload

---
+++ breaks.java
@@ -0,0 +1,11 @@
+public class breaks {
+    public static void main(String[] args) {
+        
+        for (int i =1;i<=20;i++){
+            if (i==6){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
